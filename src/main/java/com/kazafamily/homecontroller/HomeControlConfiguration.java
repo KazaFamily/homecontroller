1e397c9953929dd2cddec8b1f130454a9bf48323
Sri <sridharkaza@yahoo.com>
2017-12-23 13:46:08 -0800

    latest edits to make work on any device

--- src/main/java/com/kazafamily/homecontroller/HomeControlConfiguration.java
+++ src/main/java/com/kazafamily/homecontroller/HomeControlConfiguration.java
@@ -2,10 +2,12 @@ package com.kazafamily.homecontroller;
 
 import javax.annotation.PostConstruct;
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Path;
+import javax.ws.rs.ext.Provider;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.wadl.internal.WadlResource;
-
+import org.reflections.Reflections;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -35,9 +37,37 @@ public class HomeControlConfiguration extends ResourceConfig {
 
 	private void registerEndpoints() {
 
-		packages("com.kazafamily.homecontroller.resources");
+		//packages("com.kazafamily.homecontroller.resources");
+		scan("com.kazafamily.homecontroller.resources");
 		register(WadlResource.class);
 	}
+	
+	/**
+	 * Get around Jersey packaged jar scanning issue working with spring-boot.
+	 * Issue: if using "java -jar" instead of "mvn springboot:run", Jersey is not able to scan packages
+	 * since they're bundled in a jar. Scanning manually and registering Resources manually gets around that issue
+	 * while allowing for running the deployment using "java -jar"
+	 *
+	 * @param packages
+	 */
+	public void scan(String... packages) {
+	    for (String pack : packages) {
+	        Reflections reflections = new Reflections(pack);
+	        reflections.getTypesAnnotatedWith(Provider.class)
+	                .parallelStream()
+	                .forEach((clazz) -> {
+	                    log.info("New provider registered: " + clazz.getName());
+	                    register(clazz);
+	                });
+	        reflections.getTypesAnnotatedWith(Path.class)
+	                .parallelStream()
+	                .forEach((clazz) -> {
+	                    log.info("New resource registered: " + clazz.getName());
+	                    register(clazz);
+	                });
+	    }
+	}
+	
 	
 	private void configureSwagger() {
 	     this.register(ApiListingResource.class);
